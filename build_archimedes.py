a6e156cf0cd14f2bb7c54d9eec4ab6892b8a3ef4
Vincent Marchetti <vmarchetti@aluminum.lan>
2024-01-07 20:29:48 -0500

    Implemented adding an annotation with "supplementing motivation"

--- build_archimedes.py
+++ build_archimedes.py
@@ -4,6 +4,7 @@ label="Archimedes"
 manifest_name="archimedes.json"
 manifest_id="https://spri-open-resources.s3.us-east-2.amazonaws.com/iiif3dtsg/manifests/20231220/" + manifest_name
 model_id="https://spri-open-resources.s3.us-east-2.amazonaws.com/iiif3dtsg/assets/archimedes9.glb"
+comment_id="https://spri-open-resources.s3.us-east-2.amazonaws.com/iiif3dtsg/assets/archimedes_theorem.x3d"
 
 import iiif_prezi3 as pz
 from kshell.iiif3d import *
@@ -20,7 +21,28 @@ anno = pz.Annotation(id=generate_local_uri(ns.manifest.id),
                   target=ns.canvas.id)   
                   
 ns.anno_page.add_item(anno)
-               
+
+commentns =  buildBasicAnnotationBody(comment_id,  X3D_MIMETYPE ) 
+
+x,y,z = (0.0, 6.0, 0.0)
+target = pz.SpecificResource(   id=generate_local_uri(ns.manifest.id),
+                                    source=ns.canvas.id,
+                                    selector={  "type": "PointSelector", 
+                                                "x": x, 
+                                                "y": y, 
+                                                "z": z
+                                              }
+                                )
+ 
+# see the IIIF Presentation 3.0 API, Section 3.5, for
+# allowed values of the motivation field in IIIF annotations                               
+anno = pz.Annotation(id=generate_local_uri(ns.manifest.id),
+                  motivation="supplementing",
+                  body=commentns.body,
+                  target=target) 
+ 
+ns.anno_page.add_item(anno)
+                              
 jsontext = ns.manifest.json(indent=2)
 
 print(jsontext)
